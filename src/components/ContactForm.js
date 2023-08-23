2c648b01654bcfc8eb2e33bc42ea9c7dfd0a3a89
Conner Matson <cnr.mtsn@gmail.com>
2023-08-23 10:00:58 -0500

    show sending indicator following form submission

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -9,6 +9,7 @@ function ContactForm() {
 	}
 	const [formData, setFormData] = useState(initialFormData)
 	const [submitted, setSubmitted] = useState(false)
+	const [loading, setLoading] = useState(false)
 	const [response, setResponse] = useState({})
 
 	const handleInputChange = e => {
@@ -22,6 +23,7 @@ function ContactForm() {
 	const handleSubmit = async e => {
 		e.preventDefault()
 		console.log("Submit form: ", formData)
+		setLoading(true)
 		try {
 			const response = await fetch("/api/contact", {
 				method: "POST",
@@ -34,64 +36,79 @@ function ContactForm() {
 
 			if (response.status === 200) {
 				setSubmitted(true)
+				setLoading(false)
 				setFormData(initialFormData)
 				const data = await response.json()
 				setResponse(data)
 			}
 		} catch (error) {
 			console.log("Error: ", error)
+			setLoading(false)
 		}
 	}
 
-	if (submitted)
-		return (
-			<div className="border-t-2 border-b-2 border-brand-red py-6 px-4 rounded-sm flex flex-col gap-4 w-full lg:w-1/3 rounded-md">
-				<p className="text-white text-center">{response.message}</p>
-			</div>
-		)
 	return (
-		<form
-			onSubmit={handleSubmit}
-			className="border-t-2 border-b-2 border-brand-red py-6 px-4 rounded-sm flex flex-col gap-4 w-full lg:w-1/3 rounded-md bg-white dark:bg-black bg-opacity-10 dark:bg-opacity-50"
-			name="Contact"
-		>
-			<h1 className="text-2xl font-bold text-white text-center">
-				{"Let's work together"}
-			</h1>
-			<input
-				onChange={handleInputChange}
-				required
-				className="w-auto p-2 bg-opacity-90 bg-white text-black focus:outline-brand-red focus:outline-2 hover:bg-opacity-90 transition-all duration-300 ease-in-out"
-				type="text"
-				name="name"
-				id="name"
-				placeholder={"name".charAt(0).toUpperCase() + "name".slice(1)}
-			/>
-			<input
-				onChange={handleInputChange}
-				required
-				className="w-auto p-2 bg-opacity-90 bg-white text-black focus:outline-brand-red focus:outline-2 hover:bg-opacity-90 transition-all duration-300 ease-in-out"
-				type="email"
-				name="email"
-				id="email"
-				placeholder={"email".charAt(0).toUpperCase() + "email".slice(1)}
-			/>
-			<textarea
-				onChange={handleInputChange}
-				required
-				className="w-auto p-2 bg-opacity-90 bg-white text-black focus:outline-brand-red focus:outline-2 hover:bg-opacity-90 transition-all duration-300 ease-in-out"
-				name="message"
-				id="message"
-				placeholder={
-					"message".charAt(0).toUpperCase() + "message".slice(1)
-				}
-			/>
-			<input
-				type="submit"
-				value="Send"
-				className="w-auto mx-auto bg-brand-blue text-white dark:bg-brand-red py-2 px-20 rounded-sm hover:brightness-110 cursor-pointer transition-all duration-300 ease-in-out"
-			/>
-		</form>
+		<div className="border-t-2 border-b-2 border-brand-red py-6 px-4 rounded-sm w-full lg:w-1/3 rounded-md">
+			{loading ||
+				(submitted && (
+					<p className="text-white text-center">
+						{loading
+							? "Sending..."
+							: submitted
+							? response.message
+							: ""}
+					</p>
+				))}
+			{!loading && !submitted && (
+				<form
+					className="flex flex-col gap-4"
+					onSubmit={handleSubmit}
+					name="Contact"
+				>
+					<h1 className="text-2xl font-bold text-white text-center">
+						{"Let's work together"}
+					</h1>
+					<input
+						onChange={handleInputChange}
+						required
+						className="w-auto p-2 bg-opacity-90 bg-white text-black focus:outline-brand-red focus:outline-2 hover:bg-opacity-90 transition-all duration-300 ease-in-out"
+						type="text"
+						name="name"
+						id="name"
+						placeholder={
+							"name".charAt(0).toUpperCase() + "name".slice(1)
+						}
+					/>
+					<input
+						onChange={handleInputChange}
+						required
+						className="w-auto p-2 bg-opacity-90 bg-white text-black focus:outline-brand-red focus:outline-2 hover:bg-opacity-90 transition-all duration-300 ease-in-out"
+						type="email"
+						name="email"
+						id="email"
+						placeholder={
+							"email".charAt(0).toUpperCase() + "email".slice(1)
+						}
+					/>
+					<textarea
+						onChange={handleInputChange}
+						required
+						className="w-auto p-2 bg-opacity-90 bg-white text-black focus:outline-brand-red focus:outline-2 hover:bg-opacity-90 transition-all duration-300 ease-in-out"
+						name="message"
+						id="message"
+						placeholder={
+							"message".charAt(0).toUpperCase() +
+							"message".slice(1)
+						}
+					/>
+					<input
+						type="submit"
+						value="Send"
+						className="w-auto mx-auto bg-brand-blue text-white dark:bg-brand-red py-2 px-20 rounded-sm hover:brightness-110 cursor-pointer transition-all duration-300 ease-in-out"
+					/>
+				</form>
+			)}
+		</div>
 	)
 }
 
