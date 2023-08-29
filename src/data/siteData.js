efd05c59a0ab92e484801ed71dc981e162721a17
Conner Matson <cnr.mtsn@gmail.com>
2023-08-29 16:03:34 -0500

    update phone number

--- src/data/siteData.js
+++ src/data/siteData.js
@@ -5,6 +5,6 @@ export const siteData = {
 		"Transforming Visions into Exquisite Homes: Your Dream, Our Expertise. Build Your Future with Sims Custom Homes Today!",
 	description:
 		"With over 20 years of experience in the construction industry, I've had the privilege of contributing to the creation of 21 distinctive homes. Guiding clients through the journey of turning their visions into reality and seeing those dreams materialize is what truly fuels my passion. Now, I'm excited to take the next step and fully commit to the realm of new home construction. This transition marks not just a professional move, but a dedication to bringing more dream homes to life.",
-	phone: "555-555-5555",
+	phone: "(816) 838-0194",
 	email: "sales@srckc.com",
 }
