19ca10083e6e49f98a0e6dd9fc489dc84ef8f9c0
Conner Matson <cnr.mtsn@gmail.com>
2023-08-29 16:01:39 -0500

    reduce padding and text size on mobile

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,13 +4,15 @@ import HighlightedText from "./HighlightedText"
 export default function Hero({ className }) {
 	return (
 		<div className={className}>
-			<h1 className="text-white text-5xl leading-[4rem]">
+			<h1 className="text-white text-3xl md:text-5xl leading-[2.5rem] md:leading-[4rem]">
 				<HighlightedText
 					text={siteData.slogan}
 					wordsToHighlight={["Vision", "Reality"]}
 				/>
 			</h1>
-			<p className="text-2xl text-gray-100">{siteData.description}</p>
+			<p className="text-xl md:text-2xl text-gray-100">
+				{siteData.description}
+			</p>
 		</div>
 	)
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,12 +9,12 @@ export default function Home() {
 		(_, i) => `/images/photo-${i + 1}.jpg`
 	)
 	return (
-		<div className="flex flex-col justify-center p-8 min-h-[90vh]">
-			<div className="flex flex-col lg:flex-row items-center justify-center w-full sm:w-[85%] mx-auto min-h-[60vh] gap-8 bg-black bg-opacity-20 dark:bg-opacity-0 p-10 lg:mt-16">
-				<Hero className="flex flex-col gap-10 items-center justify-center w-full lg:w-1/2 text-center" />
+		<div className="flex flex-col justify-center p-4 md:p-8 min-h-[90vh] w-full md:w-[90%] mx-auto">
+			<div className="flex flex-col lg:flex-row items-center justify-center min-h-[60vh] gap-8 bg-black bg-opacity-20 dark:bg-opacity-0 py-6 lg:mt-16">
+				<Hero className="flex flex-col gap-10 items-center justify-center text-center" />
 				<ContactForm />
 			</div>
-			<div className="flex flex-col justify-center gap-6 w-full md:w-[80%] mx-auto mt-20">
+			<div className="flex flex-col justify-center gap-6 mt-20">
 				<h2 className="w-full border-b-2 border-white text-3xl text-white pb-4">
 					Some of our work
 				</h2>
