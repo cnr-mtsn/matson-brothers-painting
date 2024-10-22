0d7ee08c4e80e5acdf0327e624d07f63177ae0de
cnr-mtsn <cnr.mtsn@gmail.com>
2024-10-22 10:49:06 -0500

    add api route for apple pay verification file

--- src/pages/api/.well-known/apple-developer-merchantid-domain-association.js
+++
@@ -1,6 +0,0 @@
-export default async function handler(req, res) {
-	// return the file from .well-known/apple-developer-merchantid-domain-association
-	return res
-		.status(200)
-		.sendFile(".well-known/apple-developer-merchantid-domain-association")
-}
